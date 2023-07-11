f7f078e2c013c3822a3c6a7e186faf982e1ce8b5
Md. Moshiul Azam <azammoshiul45@gmail.com>
2023-07-11 18:59:57 +0600

    react router added

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,12 +1,15 @@
 import React from "react";
 import UseState from "./use-state/useState";
 import "../../style/body/body.css";
+import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
 
 const Body = () => {
   return (
-    <div>
-      <UseState />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/use-state" element={<UseState />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 

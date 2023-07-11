a5c05d09065e57c9b085011e063a3936190c34fd
Md. Moshiul Azam <azammoshiul45@gmail.com>
2023-07-11 17:19:25 +0600

    folder structure done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import StateSmaple from "./components/use-state/useState";
+
+function App() {
+  return (
+    <div className="App">
+      <StateSmaple />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/body/use-state/useState.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const StateSmaple = () => {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      {counter}
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  );
+};
+
+export default StateSmaple;
